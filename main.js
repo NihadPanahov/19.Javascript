46571a328b82e8905ea460d8bccfe3ad3370a2a9
NihadPanahov <nihadpanah03@gmail.com>
2022-08-30 15:17:56 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,48 @@
+// const dishes = ['cup','plate','spoon','knife','fork']
+// for ( let element of dishes) {
+//     console.log(element);
+// }
+
+
+// const items = ['pomidor','sogan','yerkoku','sarimsaq','semeni']
+// for ( let element of items){
+//     if (items) {
+        
+//     }
+// }
+
+
+
+// var sum=0; 
+// for(var i=0;i<=100;i++) sum+=i; 
+// console.log(sum);
+
+
+// 5-ci sual
+
+// for (let i = 10000; i < 0; i--) {
+//     if (i) {
+//             i % 9999 == 0
+//         return
+//             console.log(i);
+//     }
+// }
+
+
+// 7-ci sual
+
+const computer = {
+    cpu: 'intel i5' ,
+    ram: '8' ,
+    gpu: 'NVIDIA' ,
+}
+
+// for (const element in computer){
+        // text += element
+//     console.log(computer);
+// }
+
+for (const x for computer){
+    text += x
+    console.log(text);
+}
